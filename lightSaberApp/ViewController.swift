29769c9ca5db03e1888be204207da8591192107d
Charlie Trenholm <charleshtrenholm@gmail.com>
2018-11-01 14:54:14 -0700

    Initial Commit

---
+++ lightSaberApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lightSaberApp
+//
+//  Created by Charlie Trenholm on 11/1/18.
+//  Copyright © 2018 team name: String. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
